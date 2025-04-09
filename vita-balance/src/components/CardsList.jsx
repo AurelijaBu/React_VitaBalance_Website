64ba8c969fb96bd60e4872739a339cb5ee5f4e04
AurelijaBu <aurelija.buividyte.stud@fasttrack.lt>
2025-04-09 16:23:49 +0300

    app.v2

--- vita-balance/src/components/CardsList.jsx
+++ vita-balance/src/components/CardsList.jsx
@@ -8,7 +8,7 @@ function CardsList(){
             id: 1,
             title: 'Consistency is Key',
             content: "Sticking to a routine is the key to long-term fitness success. Whether it's strength training, yoga, or cardio, small daily efforts lead to big results over time. The best workout is the one you can do consistently!",
-            image: 'consistensy.jpg',
+            image: '/images/consistensy.jpg',
             imageTitle: "Close-up of a woman's waist being measured with a tape. Fitness and health concept. Stock Photo",
             layout: 'text-first',
         },
@@ -16,7 +16,7 @@ function CardsList(){
             id: 2,
             title: 'Fuel Your Performance',
             content: "What you eat impacts how you train! A well-balanced diet rich in protein, healthy fats, and complex carbs fuels your energy, supports muscle growth, and aids recovery. Nutrition and fitness go hand in hand—choose wisely!",
-            image: 'healthy_meal.jpg',
+            image: '/images/healthy_meal.jpg',
             imageTitle: "A delicious and healthy breakfast bowl topped with bananas, blueberries, and walnuts on a white surface. Stock Photo",
             layout: 'text-first',
             styleId: 'cardSmallScreen' 
@@ -25,7 +25,7 @@ function CardsList(){
             id: 2,
             title: 'Fuel Your Performance',
             content: "What you eat impacts how you train! A well-balanced diet rich in protein, healthy fats, and complex carbs fuels your energy, supports muscle growth, and aids recovery. Nutrition and fitness go hand in hand—choose wisely!",
-            image: 'healthy_meal.jpg',
+            image: '/images/healthy_meal.jpg',
             imageTitle: "A delicious and healthy breakfast bowl topped with bananas, blueberries, and walnuts on a white surface. Stock Photo",
             layout: 'image-first',
             styleId: 'cardWideScreen' 
@@ -34,7 +34,7 @@ function CardsList(){
             id: 3,
             title: 'Mind Over Muscle',
             content: "Fitness is not just about physical strength—it's about mental resilience too. Pushing through discomfort, staying disciplined, and embracing challenges all contribute to success. Your body achieves what your mind believes!",
-            image: 'motivation.jpg',
+            image: '/images/motivation.jpg',
             imageTitle: "Male athlete with prosthetic leg sitting in a sunlit gym, embodying strength and determination. Stock Photo",
             layout: 'text-first',
         }
